9fa5bdfc0f7850dd5ae04db477950c8e65869297
twismdx <twism@live.com>
2024-02-25 17:44:42 +1030

    a

--- src/components/SvgRmu.js
+++ src/components/SvgRmu.js
@@ -5,260 +5,336 @@ import { useGlobalContext } from './Context'
 const SvgRmu = ({ stats }) => {
 	return (
 		<svg
-			width='40%'
-			viewBox='0 0 3331 902'
-			fill='none'
+			id='Layer_1'
+			data-name='Layer 1'
 			xmlns='http://www.w3.org/2000/svg'
+			xmlnsXlink='http://www.w3.org/1999/xlink'
+			viewBox='0 0 256.58 80.75'
 			style={{
-				transform: 'scaleY(2.0)scaleX(1.4)',
+				transform: 'scaleY(1.0)scaleX(1.0)',
 				// marginLeft: '150px',
 				// marginTop: '60px',
 			}}
 		>
-			<g
-				id='Asset 2 1'
-				clip-path='url(#clip0_0_1)'
-			>
-				<g id='Layer 2'>
-					<g id='Layer 1'>
-						<path
-							id='Vector'
-							d='M249.3 19.7999C251.402 13.9857 255.249 8.96294 260.315 5.41872C265.381 1.87449 271.418 -0.018086 277.6 -0.000117368H3300.1C3304.93 -0.00391485 3309.69 1.15776 3313.97 3.38622C3318.25 5.61468 3321.94 8.84408 3324.7 12.8001C3327.47 16.7561 3329.24 21.3218 3329.87 26.1094C3330.5 30.897 3329.96 35.765 3328.3 40.2999L3090.5 690.3C3088.39 696.079 3084.55 701.069 3079.5 704.593C3074.46 708.117 3068.45 710.005 3062.3 710H42.7001C37.8838 709.998 33.1388 708.836 28.8662 706.613C24.5937 704.39 20.9191 701.171 18.1532 697.228C15.3873 693.285 13.6113 688.735 12.9755 683.96C12.3396 679.186 12.8626 674.329 14.5001 669.8L249.3 19.7999Z'
-							fill='#1C1C1C'
-						/>
-						<path
-							id='Vector_2'
-							d='M0 769C0 761.044 3.16071 753.413 8.7868 747.787C14.4129 742.161 22.0435 739 30 739H3031C3038.96 739 3046.59 742.161 3052.21 747.787C3057.84 753.413 3061 761.044 3061 769V872C3061 879.956 3057.84 887.587 3052.21 893.213C3046.59 898.839 3038.96 902 3031 902H30C22.0435 902 14.4129 898.839 8.7868 893.213C3.16071 887.587 0 879.956 0 872L0 769Z'
-							fill='#1C1C1C'
-						/>
-						{/* <path
-							id='EXT'
-							d='M2890 746H3025C3032.96 746 3040.59 749.161 3046.21 754.787C3051.84 760.413 3055 768.044 3055 776V864C3055 871.956 3051.84 879.587 3046.21 885.213C3040.59 890.839 3032.96 894 3025 894H2890V746Z'
-							fill='#42D70E'
-						/> */}
-						{/* <path
-							id='progress bar'
-							d='M2490 761C2490 757.022 2491.58 753.206 2494.39 750.393C2497.21 747.58 2501.02 746 2505 746H2890V894H2505C2501.02 894 2497.21 892.42 2494.39 889.607C2491.58 886.794 2490 882.978 2490 879V761Z'
-							fill='white'
-						/> */}
-						<path
-							id='Vector_5'
-							d='M2273.6 14.2999C2273.95 13.3335 2274.59 12.4983 2275.43 11.9078C2276.27 11.3173 2277.27 11.0003 2278.3 10.9999H2828.8C2829.61 10.9941 2830.42 11.1876 2831.14 11.5636C2831.86 11.9395 2832.49 12.4865 2832.95 13.1569C2833.41 13.8272 2833.7 14.6007 2833.8 15.4099C2833.9 16.2191 2833.79 17.0396 2833.5 17.7999L2576.4 698.8C2576.03 699.747 2575.39 700.561 2574.55 701.133C2573.71 701.705 2572.72 702.007 2571.7 702H2024.2C2023.4 702 2022.61 701.806 2021.89 701.435C2021.18 701.065 2020.57 700.528 2020.11 699.871C2019.65 699.214 2019.35 698.456 2019.25 697.66C2019.14 696.864 2019.23 696.055 2019.5 695.3L2273.6 14.2999Z'
-							fill='url(#paint0_linear_0_1)'
-						/>
-						<path
-							id='Vector_6'
-							d='M1186.7 770L1187 870'
-							stroke='#C2C2C2'
-							stroke-width='5'
-							stroke-linecap='round'
-						/>
-						<path
-							id='Vector_7'
-							d='M1816.7 770L1817 870'
-							stroke='#C2C2C2'
-							stroke-width='5'
-							stroke-linecap='round'
-						/>
-					</g>
-					<g id='Recognition_TEXT_DETECTION'>
-						{/* <g id='EXT'>
-							<path
-								id='Vector_8'
-								d='M2905.43 846L2913.88 795.14H2948.17L2946.68 804H2923.15L2921.15 816.13H2942.92L2941.43 824.99H2919.66L2917.66 837.13H2941.29L2939.8 846H2905.43Z'
-								fill='white'
-							/>
-							<path
-								id='Vector_9'
-								d='M2966.49 795.13L2973.87 812.46H2974.26L2987.46 795.13H3000L2980.29 820.56L2991.86 846H2979.73L2972.18 828.64H2971.78L2958.46 846H2945.88L2966.09 820.57L2954.63 795.14L2966.49 795.13Z'
-								fill='white'
-							/>
-							<path
-								id='Vector_10'
-								d='M3003.31 804L3004.8 795.14H3046.6L3045.11 804H3029.53L3022.53 846H3011.9L3018.85 804H3003.31Z'
-								fill='white'
-							/>
-						</g> */}
-					</g>
-				</g>
-				<text
-					id='homescore'
-					transform='translate(2450.35 290)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 240,
-						fill: '#000',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].homescore : ''}
-				</text>
-				<text
-					id=' awayscore'
-					transform='translate(2350.66 575)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 240,
-						fill: '#000',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].awayscore : ''}
-				</text>
-				<text
-					id=' homepoints'
-					transform='translate(3000 290)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 240,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].homepoints : ''}
-				</text>
-				<text
-					id=' awaypoints'
-					transform='translate(2900 575)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 240,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].awaypoints : ''}
-				</text>
-				<text
-					id=' homeplayer'
-					transform='translate(1175.72 265)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 170,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].hometeamlabel : ''}
-				</text>
-				<text
-					id=' awayplayre'
-					transform='translate(1075 550)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 170,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].awayteamlabel : ''}
-				</text>
-				<text
-					id=' tournament'
-					transform='translate(600 858)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 100,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].compname : ''}
-				</text>
-				<text
-					id=' race'
-					transform='translate(1500.89 858)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 100,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{'Race To '}
-					{stats[0] ? stats[0].raceto : ''}
-				</text>
-				<text
-					id=' race'
-					transform='translate(2135.51 858)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 100,
-						fill: '#fff',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					{stats[0] ? stats[0].venuename : ''}
-				</text>
-				{/* <text
-					id=' timer'
-					transform='translate(2135.51 865)'
-					style={{
-						isolation: 'isolate',
-						fontSize: 130,
-						fill: 'red',
-						fontFamily: 'xxBold',
-						fontWeight: '900',
-						textAlign: 'center',
-						textAnchor: 'middle',
-					}}
-				>
-					1:00
-				</text> */}
-			</g>
 			<defs>
+				<clipPath id='clippath'>
+					<rect
+						y='14.46'
+						width='252.15'
+						height='66.29'
+						style={{ fill: 'none', strokeWidth: '0px' }}
+					/>
+				</clipPath>
 				<linearGradient
-					id='paint0_linear_0_1'
-					x1='2912.84'
-					y1='49.0399'
-					x2='1974.34'
-					y2='640.54'
+					id='linear-gradient'
+					x1='609.54'
+					y1='986.21'
+					x2='-328.96'
+					y2='394.71'
+					gradientTransform='translate(176.69 90.55) scale(.07 -.07)'
 					gradientUnits='userSpaceOnUse'
 				>
 					<stop
-						offset='0.24'
-						stop-color='#F8F5F5'
+						offset='.24'
+						stop-color='#f8f5f5'
 					/>
 					<stop
-						offset='0.77'
-						stop-color='#BBBBBB'
+						offset='.77'
+						stop-color='#bbb'
 					/>
 				</linearGradient>
-				<clipPath id='clip0_0_1'>
-					<rect
-						width='3330.13'
-						height='902'
-						fill='white'
-					/>
-				</clipPath>
 			</defs>
+			<g style={{ clipPath: 'url(#clippath)' }}>
+				<g
+					id='Asset_2_1'
+					data-name='Asset 2 1'
+				>
+					<g
+						id='Layer_2'
+						data-name='Layer 2'
+					>
+						<g
+							id='Layer_1-2'
+							data-name='Layer 1'
+						>
+							<path
+								id='Vector'
+								d='M25.74,15.92c.15-.43.44-.8.81-1.06.37-.26.82-.4,1.27-.4h222.13c.35,0,.7.09,1.02.25.31.16.59.4.79.69.2.29.33.63.38.98.05.35,0,.71-.12,1.04l-17.48,47.77c-.16.42-.44.79-.81,1.05-.37.26-.81.4-1.26.4H10.55c-.35,0-.7-.09-1.02-.25-.31-.16-.58-.4-.79-.69-.2-.29-.33-.62-.38-.98-.05-.35,0-.71.11-1.04L25.74,15.92Z'
+								style={{ fill: '#1c1c1c', strokeWidth: '0px' }}
+							/>
+							<path
+								d='M6.47,70.9c.05-.14.1-.27.14-.39s.09-.24.14-.37c.06-.13.13-.26.21-.38s.18-.23.28-.34c.1-.1.22-.2.34-.28s.25-.15.38-.2.27-.1.41-.13.29-.04.44-.04h222.1c.15,0,.29.01.44.04s.28.07.41.13.26.12.38.2.23.17.34.28c.1.1.22.24.32.4s.19.34.24.53c.07.19.09.38.1.56s-.02.35-.03.49l-.86,2.34-.86,2.34-.33.9-.33.9c-.32.86-.89,1.58-1.61,2.09s-1.6.79-2.53.79H6.25c-1.63,0-2.76-1.62-2.21-3.15l2.42-6.71Z'
+								style={{ fill: '#1c1c1c', strokeWidth: '0px' }}
+							/>
+							<path
+								d='M212.14,79.86l4.06-10.38s9.65,0,14.41,0c1.26,0,2.13,1.25,1.7,2.43-.62,1.68-1.44,3.91-2.05,5.59-.53,1.45-1.92,2.41-3.46,2.41l-14.65-.05Z'
+								style={{ fill: '#42d70e', strokeWidth: '0px' }}
+							/>
+							<path
+								id='progress_bar'
+								data-name='progress bar'
+								d='M216.18,69.48l-4.04,10.38h-36.36c-1.03,0-1.71-1.08-1.26-2.01l3.44-7.02c.34-.69,1.04-1.13,1.8-1.13l36.41-.22Z'
+								style={{ fill: '#fff', strokeWidth: '0px' }}
+							/>
+							<path
+								id='Vector_5'
+								data-name='Vector 5'
+								d='M174.51,15.52c.03-.07.07-.13.13-.18.06-.04.14-.07.21-.07h40.46c.06,0,.12.01.17.04.05.03.1.07.13.12.03.05.06.11.06.17,0,.06,0,.12-.02.18l-18.89,50.05c-.03.07-.07.13-.14.17-.06.04-.13.06-.21.06h-40.24c-.06,0-.12-.01-.17-.04-.05-.03-.1-.07-.13-.11-.03-.05-.06-.1-.06-.16,0-.06,0-.12.02-.17l18.67-50.05Z'
+								style={{
+									fill: 'url(#linear-gradient)',
+									strokeWidth: '0px',
+								}}
+							/>
+						</g>
+						<g
+							id='Recognition_span_DETECTION'
+							data-name='Recognition span DETECTION'
+						>
+							<g id='EXT'>
+								<path
+									id='Vector_8'
+									data-name='Vector 8'
+									d='M217.33,76.83l.62-3.74h2.52l-.11.65h-1.73l-.15.89h1.6l-.11.65h-1.6l-.15.89h1.74l-.11.65h-2.53Z'
+									style={{ fill: '#fff', strokeWidth: '0px' }}
+								/>
+								<path
+									id='Vector_9'
+									data-name='Vector 9'
+									d='M221.82,73.09l.54,1.27h.03l.97-1.27h.92l-1.45,1.87.85,1.87h-.89l-.55-1.28h-.03l-.98,1.28h-.92l1.49-1.87-.84-1.87h.87Z'
+									style={{ fill: '#fff', strokeWidth: '0px' }}
+								/>
+								<path
+									id='Vector_10'
+									data-name='Vector 10'
+									d='M224.52,73.74l.11-.65h3.07l-.11.65h-1.15l-.51,3.09h-.78l.51-3.09h-1.14Z'
+									style={{ fill: '#fff', strokeWidth: '0px' }}
+								/>
+							</g>
+						</g>
+					</g>
+				</g>
+			</g>
+			<path
+				d='M29.92,2.13c.05-.14.1-.27.14-.39s.09-.24.14-.37c.06-.13.13-.26.21-.38s.18-.23.28-.34c.1-.1.22-.2.34-.28s.25-.15.38-.2.27-.1.41-.13.29-.04.44-.04h111.05s55.52,0,55.52,0h55.52c.15,0,.29.01.44.04s.28.07.41.13.26.12.38.2.23.17.34.28c.1.1.22.24.32.4s.19.34.24.53c.07.19.09.38.1.56s-.02.35-.03.49l-.86,2.34-.86,2.34-.33.9-.33.9c-.32.86-.89,1.58-1.61,2.09s-1.6.79-2.53.79h-108.89s-111.44,0-111.44,0c-1.63,0-2.76-1.62-2.21-3.15l2.42-6.71Z'
+				style={{ fill: '#1c1c1c', strokeWidth: '0px' }}
+			/>
+			<path
+				d='M22.89,23.79l1.29-3.58'
+				style={{ fill: '#1c1c1c', opacity: '.25', strokeWidth: '0px' }}
+			/>
+			<path
+				d='M248.72,26.44l3.43-9.42-3.43,9.42Z'
+				style={{ fill: 'none', strokeWidth: '0px' }}
+			/>
+			<path
+				id='Vector_7'
+				data-name='Vector 7'
+				d='M143.25,70.93l-2.59,7.32'
+				style={{
+					fill: 'none',
+					stroke: '#c2c2c2',
+					strokeLinecap: 'round',
+					strokeMiterlimit: '10',
+				}}
+			/>
+			<path
+				id='Vector_7-2'
+				data-name='Vector 7'
+				d='M138,2.37l-2.59,7.32'
+				style={{
+					fill: 'none',
+					stroke: '#c2c2c2',
+					strokeLinecap: 'round',
+					strokeMiterlimit: '10',
+				}}
+			/>
+			<path
+				id='Vector_7-3'
+				data-name='Vector 7'
+				d='M90.83,70.57l-2.59,7.32'
+				style={{
+					fill: 'none',
+					stroke: '#c2c2c2',
+					strokeLinecap: 'round',
+					strokeMiterlimit: '10',
+				}}
+			/>
+			<text
+				id='Timer'
+				transform='translate(148, 78.5)'
+				style={{
+					fontSize: 10,
+					fill: 'red',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					spanAlign: 'center',
+					spanAnchor: 'middle',
+					fontVariant: 'small-caps',
+				}}
+			>
+				{'1:00'}
+			</text>
+			<text
+				id='race_to'
+				data-name='race to'
+				transform='translate(109, 78)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 8,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					fontVariant: 'small-caps',
+					textAnchor: 'middle',
+				}}
+			>
+				{'Race To'}
+				{/* {stats[0] ? stats[0]?.raceto : 'Race To '} */}
+			</text>
+			<text
+				id='tournament_name'
+				data-name='tournament name'
+				transform='translate(195, 9)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 8,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '700',
+					textAlign: 'center',
+					textAnchor: 'middle',
+					fontVariant: 'small-caps',
+				}}
+			>
+				{stats[0] ? stats[0].compname : ''}
+			</text>
+			<text
+				id='Venue'
+				transform='translate(47, 78)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 8,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+					fontVariant: 'small-caps',
+				}}
+			>
+				{stats[0] ? stats[0].venuename : ''}
+			</text>
+			<text
+				id='Timezone'
+				transform='translate(82, 9)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 8,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+					fontVariant: 'small-caps',
+				}}
+			>
+				{stats[0] ? stats[0].timezone : ''}
+			</text>
+			<text
+				id='home_name'
+				data-name='home name'
+				transform='translate(95, 32)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 15,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].hometeamlabel : ''}
+			</text>
+			<text
+				id='away_name'
+				data-name='away name'
+				transform='translate(87, 59)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 15,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].awayteamlabel : ''}
+			</text>
+			<text
+				id='homescore'
+				transform='translate(190, 32)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 18,
+					fill: '#000',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].homescore : ''}
+			</text>
+			<text
+				id='awayscore'
+				transform='translate(180, 59)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 18,
+					fill: '#000',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].awayscore : ''}
+			</text>
+			<text
+				id='home_points'
+				data-name='home points'
+				transform='translate(230, 32)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 18,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].homescorepoints : ''}
+			</text>
+			<text
+				id='away_points'
+				data-name='away points'
+				transform='translate(220, 59)'
+				style={{
+					isolation: 'isolate',
+					fontSize: 18,
+					fill: '#fff',
+					fontFamily: 'xxBold',
+					fontWeight: '900',
+					textAlign: 'center',
+					textAnchor: 'middle',
+				}}
+			>
+				{stats[0] ? stats[0].awayscorepoints : ''}
+			</text>
 		</svg>
 	)
 }
